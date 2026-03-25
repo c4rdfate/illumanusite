585c3ca7f58f24c39fca8a62ac9243770210851b
Leviathan Shoates <147337325+c4rdfate@users.noreply.github.com>
2026-03-25 17:40:41 -0400

    test with random code

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+// var declaration 
+xpos = 1; 
+xspeed = 2; 
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+ // background(220);
+  
+  fill("purple");
+  ellipse(xpos, height/2, width/3, height/3);
+  
+  if(xpos > width) {
+    xspeed = -5; 
+    }
+    
+  if (xpos <  2){
+    xspeed = 5;
+  }
+    
+  xpos += xspeed;
 }
