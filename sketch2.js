9f27f2f06b8e37f5ab977bab3bf49f9b23394a3d
Leviathan Shoates <147337325+c4rdfate@users.noreply.github.com>
2026-03-26 18:19:59 -0400

    main page changes

--- sketch2.js
+++ sketch2.js
@@ -1,14 +1,23 @@
+//svg declarations 
+let imgsidebar; //not used yet 
+let imgframe; //not used yet 
+let imgbbar; //not used yet 
+let imgfullsized; //not used yet 
+let img1; //not used yet
+
+function preload() {
+  img1 = loadImage('test_assets/frame.svg');
+
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight); //not needed for now, will be used for testing purposes 
 
 
 }
 
 function draw() {
-  
-  background('blue');
-  
-
+   
   
 }
 
