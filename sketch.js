c8b87bae28a504bc750808b78a82f6f73542d1ef
Leviathan Shoates <147337325+c4rdfate@users.noreply.github.com>
2026-04-08 18:40:46 -0400

    cleaned up sections, can't remove extra space from the end of the file, but it doesn't affect anything.

--- sketch.js
+++ sketch.js
@@ -11,17 +11,16 @@ function setup() {
   
   //enter button set up 
 
- let button = createButton('Enter');
+  let button = createButton('Enter');
   button.position(windowWidth/1.4,windowHeight/1.7)
   button.mousePressed(buttonPressed);
   function buttonPressed() {
-  window.location.href = "main.html";
-}
+  window.location.href = "main.html"; }
 }
 
 function draw() {
-    background('red');
+   background('red');
   
-  image(img1, windowWidth/2, 0);
+ image(img1, windowWidth/2, 0);
 
 }
--- sketch2.js
+++ sketch2.js
@@ -11,7 +11,7 @@ function preload() {
 }
 
 function setup() {
-  //createCanvas(windowWidth, windowHeight); //not needed for now, will be used for testing purposes 
+ // createCanvas(windowWidth, windowHeight); //not needed for now, will be used for testing purposes 
 
 
 }
